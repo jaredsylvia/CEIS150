e6d73e20b2d8c0b6056d3f8d75a6c8d356ddb768
jaredsylvia <jaredsylvia@gmail.com>
2022-05-03 11:31:07 -0700

    Initial commit

---
+++ week1Discussion.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 11:23:41 2022
+
+@author: jared
+"""
+
+userName = input('What is your name?')
+userAge = input('How old are you?')
+userColor = input('What is your favorite color?')
+
+print('Name:', userName, '\nAge:', userAge, '\nFavorite Color:', userColor)
