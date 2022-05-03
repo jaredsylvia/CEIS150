9d382b2178567d09aa86b1b4f3e5168158343c76
jaredsylvia <jaredsylvia@gmail.com>
2022-05-03 11:33:22 -0700

    Update week1Discussion.py

--- week1Discussion.py
+++ week1Discussion.py
@@ -5,8 +5,8 @@ Created on Tue May  3 11:23:41 2022
 @author: jared
 """
 
-userName = input('What is your name?')
-userAge = input('How old are you?')
-userColor = input('What is your favorite color?')
+userName = str(input('What is your name?'))
+userAge = str(input('How old are you?'))
+userColor = str(input('What is your favorite color?'))
 
 print('Name:', userName, '\nAge:', userAge, '\nFavorite Color:', userColor)
