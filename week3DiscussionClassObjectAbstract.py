e95008c6e7d8fd93acd6ee54ef154bab356a4104
jaredsylvia <jaredsylvia@gmail.com>
2022-05-16 18:43:24 -0700

    Made it more proper?

--- week3DiscussionClassObjectAbstract.py
+++ week3DiscussionClassObjectAbstract.py
@@ -44,48 +44,48 @@ class myClass:
             'myList': self.myList})
         
     
-        
-# Generate object with self.name of 'First Object', and a series of random 
-# integers between 1 and 20        
-myObject = myClass('First Object',random.randint(1,20),random.randint(1,20), 
-                   random.randint(1,20),random.randint(1,20),
-                   random.randint(1,20),random.randint(1,20))
-
-# Generate object with self.name of 'Second Object', and a shorter 
-# series of random integers between 1 and 20        
-myObject2 = myClass('Second Object',random.randint(1,20),
-                      random.randint(1,20),random.randint(1,20),
-                      random.randint(1,20))
-
-# Set myFinalObject to be the object returned from the gt comparison
-myFinalObject = myObject > myObject2
-
-# Display values for myObject as dictionary called from getInfo function
-print('*** First Object ***')
-print(myObject.getInfo())
-
-# Display values for myObject2
-print('*** Second Object ***')
-print(myObject2.getInfo())
-
-# Display boolean operator returned from __eq__
-print('*** Are object myVar1 values equal or not equal: ***')
-print('Equal: {}'.format(myObject == myObject2))
-print('Not Equal: {}'.format(myObject != myObject2))
-
-
-#Display values for final object, determined by gt comparison
-print('*** Final Object ***')
-print(myFinalObject.getInfo())
-
-#Display shorter list based on lt comparison in both directions
-print('*** Shorter List ***')
-print(myObject < myObject2)
-print(myObject2 < myObject)
-
-#Display string output of myClass for each object
-print('*** String output of objects ***')
-print(myObject)
-print(myObject2)
-print(myFinalObject)
+if __name__ == "__main__":        
+    # Generate object with self.name of 'First Object', and a series of random 
+    # integers between 1 and 20        
+    myObject = myClass('First Object',random.randint(1,20),random.randint(1,20), 
+                       random.randint(1,20),random.randint(1,20),
+                       random.randint(1,20),random.randint(1,20))
+    
+    # Generate object with self.name of 'Second Object', and a shorter 
+    # series of random integers between 1 and 20        
+    myObject2 = myClass('Second Object',random.randint(1,20),
+                          random.randint(1,20),random.randint(1,20),
+                          random.randint(1,20))
+    
+    # Set myFinalObject to be the object returned from the gt comparison
+    myFinalObject = myObject > myObject2
+    
+    # Display values for myObject as dictionary called from getInfo function
+    print('*** First Object ***')
+    print(myObject.getInfo())
+    
+    # Display values for myObject2
+    print('*** Second Object ***')
+    print(myObject2.getInfo())
+    
+    # Display boolean operator returned from __eq__
+    print('*** Are object myVar1 values equal or not equal: ***')
+    print('Equal: {}'.format(myObject == myObject2))
+    print('Not Equal: {}'.format(myObject != myObject2))
+    
+    
+    #Display values for final object, determined by gt comparison
+    print('*** Final Object ***')
+    print(myFinalObject.getInfo())
+    
+    #Display shorter list based on lt comparison in both directions
+    print('*** Shorter List ***')
+    print(myObject < myObject2)
+    print(myObject2 < myObject)
+    
+    #Display string output of myClass for each object
+    print('*** String output of objects ***')
+    print(myObject)
+    print(myObject2)
+    print(myFinalObject)
 
--- week3DiscussionClassObjectInventory.py
+++ week3DiscussionClassObjectInventory.py
@@ -46,21 +46,22 @@ class item:
     def __str__(self):
         return(f'item({self.name},{self.description},{self.upc},{self.employee}\n'
                f'{self.vendor}\n')
-    
-# Create two separate employees both named Dave    
-employee1 = user('Dave', 101)
-employee2 = user('Dave', 105)
 
-# employee1 is in charge of adding vendors to the system
-vendor1 = vendor('Spacely Sprockets', '123 E. Main', 
-                 '(800)555-1212', 1001, employee1)
-vendor2 = vendor('Cogswell Cogs', '345 W. First',
-                 '(888)555-1212', 1008, employee1)
-
-# employee2 is in charge of adding items to the system
-item1 = item('Cog', 'A Cog', 'ABC123', 5.99, vendor1, employee2)
-item2 = item('Sprocket', 'A Sprocket', 'ABC456', 6.99, vendor1, employee2)
-item3 = item('Cog', 'A Cog', 'DEF123', 5.49, vendor2, employee2)
-item4 = item('Sprocket', 'A Sprocket', 'DEF456', 6.49, vendor2, employee2)
+if __name__ == "__main__":    
+    # Create two separate employees both named Dave    
+    employee1 = user('Dave', 101)
+    employee2 = user('Dave', 105)
     
-print(item1)        
+    # employee1 is in charge of adding vendors to the system
+    vendor1 = vendor('Spacely Sprockets', '123 E. Main', 
+                     '(800)555-1212', 1001, employee1)
+    vendor2 = vendor('Cogswell Cogs', '345 W. First',
+                     '(888)555-1212', 1008, employee1)
+    
+    # employee2 is in charge of adding items to the system
+    item1 = item('Cog', 'A Cog', 'ABC123', 5.99, vendor1, employee2)
+    item2 = item('Sprocket', 'A Sprocket', 'ABC456', 6.99, vendor1, employee2)
+    item3 = item('Cog', 'A Cog', 'DEF123', 5.49, vendor2, employee2)
+    item4 = item('Sprocket', 'A Sprocket', 'DEF456', 6.49, vendor2, employee2)
+        
+    print(item1)        
